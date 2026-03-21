e356adf59ce5cd56f40d7de56fe0dca9bba804c0
Mike Roberts <mike@symphonia.io>
2026-03-21 10:39:10 -0400

    Add some utility elements

--- test/hiccoughElements.test.ts
+++ test/hiccoughElements.test.ts
@@ -16,10 +16,14 @@ import {
   HiccoughElement,
   htmlPage,
   i,
+  includeCss,
+  includeJs,
   input,
   label,
   link,
+  mailTo,
   meta,
+  orderedList,
   p,
   script,
   table,
@@ -28,7 +32,8 @@ import {
   th,
   thead,
   title,
-  tr
+  tr,
+  unorderedList
 } from '../src/index.js'
 import { br, hr, img, span } from '../src/hiccoughElements.js'
 
@@ -88,6 +93,55 @@ test('link element', () => {
 
 test('a element', () => {
   expect(a('/path', 'click me')).toMatchObject({ name: 'a', attributes: { href: '/path' }, content: ['click me'] })
+})
+
+test('mailTo element', () => {
+  expect(mailTo('user@example.com')).toMatchObject({
+    name: 'a',
+    attributes: { href: 'mailto:user@example.com' },
+    content: ['user@example.com']
+  })
+  expect(mailTo('user@example.com', 'Contact Us')).toMatchObject({
+    name: 'a',
+    attributes: { href: 'mailto:user@example.com' },
+    content: ['Contact Us']
+  })
+})
+
+test('unorderedList element', () => {
+  expect(unorderedList(['a', 'b'])).toEqual({
+    isElement: true,
+    name: 'ul',
+    content: [
+      { isElement: true, name: 'li', content: ['a'] },
+      { isElement: true, name: 'li', content: ['b'] }
+    ]
+  })
+})
+
+test('orderedList element', () => {
+  expect(orderedList(['a', 'b'])).toEqual({
+    isElement: true,
+    name: 'ol',
+    content: [
+      { isElement: true, name: 'li', content: ['a'] },
+      { isElement: true, name: 'li', content: ['b'] }
+    ]
+  })
+})
+
+test('includeJs', () => {
+  expect(includeJs('app.js')).toEqual([
+    { isElement: true, name: 'script', attributes: { type: 'text/javascript', src: 'app.js' }, content: [] }
+  ])
+  expect(includeJs('app.js', 'vendor.js')).toHaveLength(2)
+})
+
+test('includeCss', () => {
+  expect(includeCss('style.css')).toEqual([
+    { isElement: true, voidElement: true, name: 'link', attributes: { type: 'text/css', href: 'style.css', rel: 'stylesheet' } }
+  ])
+  expect(includeCss('style.css', 'theme.css')).toHaveLength(2)
 })
 
 test('nested table elements', () => {
